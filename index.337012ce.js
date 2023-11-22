79978312d3661e904185b10a75d62c0b299ebaac
Alex Rog <sasharg303@gmail.com>
2023-11-22 15:50:13 +0100

    make build

---
+++ index.337012ce.js
@@ -0,0 +1,2 @@
+var r=Array.from(document.querySelectorAll("tr")),e=!0,l=!1,n=void 0;try{for(var t,o=r[Symbol.iterator]();!(e=(t=o.next()).done);e=!0){var a=t.value,i=a.children[1].cloneNode(!0);a.insertBefore(i,a.children[a.children.length-1])}}catch(r){l=!0,n=r}finally{try{e||null==o.return||o.return()}finally{if(l)throw n}}
+//# sourceMappingURL=index.337012ce.js.map
